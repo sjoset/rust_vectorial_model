6fa0292f28ca14e461b0d79004e375dbd08ba9bb
Shawn O <szo0032@auburn.edu>
2024-06-30 20:11:00 -0500

    Update flake using flake template for rust

--- flake.nix
+++ flake.nix
@@ -1,62 +1,22 @@
-  {
+{
   inputs = {
-    naersk.url = "github:nmattia/naersk/master";
-    # This must be the stable nixpkgs if you're running the app on a
-    # stable NixOS install.  Mixing EGL library versions doesn't work.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = github:edolstra/flake-compat;
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, utils, naersk, ... }:
+  outputs = { self, nixpkgs, utils, naersk }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-        libPath = with pkgs; lib.makeLibraryPath [
-          libGL
-          libxkbcommon
-          wayland
-          xorg.libX11
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXrandr
-        ];
       in
       {
-        defaultPackage = naersk-lib.buildPackage {
-          src = ./.;
-          doCheck = true;
-          pname = "sixty-two";
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          buildInputs = with pkgs; [
-            xorg.libxcb
-          ];
-          postInstall = ''
-            wrapProgram "$out/bin/sixty-two" --prefix LD_LIBRARY_PATH : "${libPath}"
-          '';
-        };
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
+        defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo
-            cargo-insta
-            pre-commit
-            rust-analyzer
-            rustPackages.clippy
-            rustc
-            rustfmt
-            tokei
-            xorg.libxcb
-          ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          LD_LIBRARY_PATH = libPath;
-          GIT_EXTERNAL_DIFF = "${difftastic}/bin/difft";
         };
-      });
+      }
+    );
 }
